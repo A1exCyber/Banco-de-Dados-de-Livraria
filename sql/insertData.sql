9e26bb5c30fd3c3b2fcda6c34f6d806fc46ef18c
Alex Gomes <alex.gsone@gmail.com>
2025-01-20 12:18:14 -0300

    Inserindo novos valores na tabela autores

--- sql/insertData.sql
+++ sql/insertData.sql
@@ -1,2 +1,8 @@
 INSERT INTO authors ("name", "bio")
 VALUES ('Eiichiro Oda', 'Eiichiro Oda em um mangaká conhecido pela criação do mangá One Piece.');
+
+INSERT INTO authors ("name", "bio")
+VALUES ('J. K. Rowling', 'J. K. Rowling é uma escritora, 
+            roteirista e produtora cinematográfica britânica, 
+                    notória por escrever a série de livros Harry Potter.');
+
