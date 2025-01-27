e6682ce3a8e0b2038c6daea85fe5573981d75503
Alex Gomes <alex.gsone@gmail.com>
2025-01-27 09:37:26 -0300

    including RETURNING *

--- sql/insertData.sql
+++ sql/insertData.sql
@@ -1,70 +1,70 @@
 /* 2.2.1 Autores*/
 INSERT INTO authors ("name", "bio")
-VALUES ('Eiichiro Oda', 'Eiichiro Oda em um mangaká conhecido pela criação do mangá One Piece.');
+VALUES ('Eiichiro Oda', 'Eiichiro Oda em um mangaká conhecido pela criação do mangá One Piece.') RETURNING *;
 
 INSERT INTO authors ("name", "bio")
 VALUES ('J. K. Rowling', 'J. K. Rowling é uma escritora, 
             roteirista e produtora cinematográfica britânica, 
-                    notória por escrever a série de livros Harry Potter.');
+                    notória por escrever a série de livros Harry Potter.') RETURNING *;
 
 INSERT INTO authors ("name", "bio")
-VALUES ('Osvaldo Silva', 'Autor e compositor brasileiro.');
+VALUES ('Osvaldo Silva', 'Autor e compositor brasileiro.') RETURNING *;
 
 /* 2.2.2 Livros*/
 INSERT INTO books ("name", "pages", "createdAt", "updatedAt", "autorId")
-VALUES ('Harry Potter', '325', '2025-01-22', '2025-01-22', 2);
+VALUES ('Harry Potter', '325', '2025-01-22', '2025-01-22', 2) RETURNING *;
 
 INSERT INTO books ("name", "pages", "createdAt", "updatedAt", "autorId")
-VALUES ('Jogos Vorazes', '276', '2025-01-23', '2025-01-23', null);
+VALUES ('Jogos Vorazes', '276', '2025-01-23', '2025-01-23', null) RETURNING *;
 
 INSERT INTO books ("name", "pages", "createdAt", "updatedAt", "autorId")
-VALUES ('One Piece - Volume 1', '120', '2025-01-23', '2025-01-23', null);
+VALUES ('One Piece - Volume 1', '120', '2025-01-23', '2025-01-23', null) RETURNING *;
 
 INSERT INTO books ("name", "pages", "createdAt", "updatedAt", "autorId")
-VALUES ('One Piece - Volume 2', '137', '2025-01-23', '2025-01-23', null);
+VALUES ('One Piece - Volume 2', '137', '2025-01-23', '2025-01-23', null) RETURNING *;
 
 /* 2.2.3 Categorias*/
 INSERT INTO categories ("name","createdAt", "updatedAt")
-VALUES ('Mangá','2025-01-24', '2025-01-24');
+VALUES ('Mangá','2025-01-24', '2025-01-24') RETURNING *;
 
 INSERT INTO categories ("name","createdAt","updatedAt")
-VALUES ('Aventura','2025-01-24','2025-01-24');
+VALUES ('Aventura','2025-01-24','2025-01-24') RETURNING *;
 
 INSERT INTO categories ("name","createdAt","updatedAt")
-VALUES ('Fantasia','2025-01-24','2025-01-24');
+VALUES ('Fantasia','2025-01-24','2025-01-24') RETURNING *;
 
 /*books_categories*/
 /*categoria aventura: */
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('1', '2');
+VALUES ('1', '2') RETURNING *;
 
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('5', '2');
+VALUES ('5', '2') RETURNING *;
 
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('6', '2');
+VALUES ('6', '2') RETURNING *;
 
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('7', '2');
+VALUES ('7', '2') RETURNING *;
 
 /*categoria fantasia: */
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('1', '3');
+VALUES ('1', '3') RETURNING *;
 
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('6', '3');
+VALUES ('6', '3') RETURNING *;
 
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('7', '3');
+VALUES ('7', '3') RETURNING *;
 
 /*categoria mangá: */
 INSERT INTO books_categories ("bookId", "categoryId")
-VALUES ('6', '1'), ('7', '1') ;
+VALUES ('6', '1'), ('7', '1') RETURNING *;
 
 
 /*2.2.5 Relacionamento entre autores e dados de contato*/
 INSERT INTO contact_infos ("phone", "email", "authorId")
-VALUES ('(44) 99123-4567', 'osvaldo@osvaldocompany.com', 3);
+VALUES ('(44) 99123-4567', 'osvaldo@osvaldocompany.com', 3) RETURNING *;
 
 
 
