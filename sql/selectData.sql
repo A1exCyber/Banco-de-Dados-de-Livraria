841c94d45aa4d58ce6f819dd169ff0021f0216a7
Alex Gomes <alex.gsone@gmail.com>
2025-02-02 23:46:22 -0300

    Update selectData.sql

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -4,5 +4,5 @@ SELECT * FROM books;
 /*Leitura de todos os livros da categoria "Fantasia".*/
 SELECT books.* 
 FROM books
-INNER JOIN categories ON books.id = 
+INNER JOIN categories ON books.id = ?
 
