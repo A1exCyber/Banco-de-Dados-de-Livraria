86307439f3ebd3f3fe498c4ac114fc4e0210e3b1
Alex Gomes <alex.gsone@gmail.com>
2025-02-04 12:04:06 -0300

    Create deleteData.sql

---
+++ sql/deleteData.sql
@@ -0,0 +1,13 @@
+/*2.5 Excluindo de dados*/
+
+/*Exclusão do livro "Jogos Vorazes".*/
+DELETE FROM books
+WHERE id = 5; 
+
+/*Exclusão da autora "J. K. Rowling".*/
+DELETE FROM authors
+WHERE id = 2; 
+
+/*Exclusão do autor "Osvaldo Silva".*/
+DELETE FROM authors
+WHERE id = 3; 
