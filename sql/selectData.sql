e0bba681390c3ac81789bdf91e69de61b8f1809d
Alex Gomes <alex.gsone@gmail.com>
2025-02-04 09:24:11 -0300

    Update selectData.sql

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -2,10 +2,21 @@
 SELECT * FROM books;
 
 /*Leitura de todos os livros da categoria "Fantasia".*/
-SELECT books.name AS book_name, categories.name AS category_name
+SELECT books.name
 FROM books
 INNER JOIN books_categories ON books.id = books_categories."bookId"
 INNER JOIN categories ON books_categories."categoryId" = categories.id
 WHERE categories.name = 'Fantasia';
 
-/**/
+/*Leitura de todos os livros com suas respectivas categorias, 
+renomeando colunas para evitar conflito entre chaves.*/
+
+SELECT books.name AS book_name, categories.name AS category_name
+FROM books
+INNER JOIN books_categories ON books.id = books_categories."bookId"
+INNER JOIN categories ON books_categories."categoryId" = categories.id;
+
+/*Leitura do livro "Harry Potter" com as informações do autor, 
+renomeando colunas para evitar conflito entre chaves.*/
+
+
