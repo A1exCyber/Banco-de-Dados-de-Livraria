4e1c473090542624b56eeb92be9538b19da7b6fe
Alex Gomes <alex.gsone@gmail.com>
2025-02-04 09:18:36 -0300

    Leitura dos livros da categoria Fantasia

--- sql/selectData.sql
+++ sql/selectData.sql
@@ -2,8 +2,10 @@
 SELECT * FROM books;
 
 /*Leitura de todos os livros da categoria "Fantasia".*/
-SELECT books.*
+SELECT books.name AS book_name, categories.name AS category_name
 FROM books
-INNER JOIN categories ON categories.id = books_categories.bookId
-INNER JOIN books_categories ON books_categories.categoryId = categories.id;
+INNER JOIN books_categories ON books.id = books_categories."bookId"
+INNER JOIN categories ON books_categories."categoryId" = categories.id
+WHERE categories.name = 'Fantasia';
 
+/**/
